0d1bf6b5ab7004d1103e6f8885b88d5c2ecfe99f
Jorge09-es <jorgehg0909@gmail.com>
2021-05-19 14:08:31 +0200

    Test

---
+++ ExamenUT5_2/src/Ej2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ej2 {
+
+	public static void main(String[] args) {
+		int num, aux;
+		Scanner sc = new Scanner(System.in);
+		
+		SumaArray[] suma = new SumaArray[3];
+		
+		for (int i = 0; i < suma.length; i++) {
+			System.out.println("Inserte un número");
+			num = sc.nextInt();
+
+		}
+		
+		
+	}
+
+}
---
+++ ExamenUT5_2/src/SumaArray.java
@@ -0,0 +1,11 @@
+
+public class SumaArray {
+ 
+	int num;
+
+	public SumaArray(int num) {
+		super();
+		this.num = num;
+	}
+	
+}
